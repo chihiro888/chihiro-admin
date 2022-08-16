24d1b8c440d8d4ec88cf3ecf354b0164750e0723
chihiro888 <93immm@naver.com>
2022-08-16 13:51:17 +0900

    feature: class-validator

--- backend/src/api/auth/dto/sign-in.dto.ts
+++ backend/src/api/auth/dto/sign-in.dto.ts
@@ -1,13 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { IsEmail, IsNotEmpty, Length } from 'class-validator'
 import PARAMETER from 'src/common/constants/parameter'
 
 export class SignInDto {
+  @Length(3, 50)
+  @IsEmail()
+  @IsNotEmpty()
   @ApiProperty({
     description: PARAMETER.ACCOUNT,
     example: PARAMETER.ACCOUNT_EXAMPLE
   })
   account: string
 
+  @Length(3, 50)
+  @IsNotEmpty()
   @ApiProperty({
     description: PARAMETER.PASSWORD,
     example: PARAMETER.PASSWORD_EXAMPLE
--- frontend/src/app/modules/auth/core/_requests.ts
+++ frontend/src/app/modules/auth/core/_requests.ts
@@ -5,9 +5,9 @@ export const GET_USER_BY_SESSION = `/api/auth/getUserBySession`
 export const LOGIN_URL = '/api/auth/signIn'
 
 // Server should return AuthModel
-export function login(email: string, password: string) {
+export function login(account: string, password: string) {
   return axios.post<CommonResponse>(LOGIN_URL, {
-    email,
+    account,
     password
   })
 }
