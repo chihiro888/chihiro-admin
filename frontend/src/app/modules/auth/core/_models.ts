d5e94e3f1aadbca8b10069c69e3b57bf471ede70
chihiro888 <93immm@naver.com>
2022-08-16 13:37:56 +0900

    feature: update _models

--- frontend/src/app/modules/auth/core/_models.ts
+++ frontend/src/app/modules/auth/core/_models.ts
@@ -52,26 +52,4 @@ export interface UserModel {
   createdAt: string | null
   updatedAt: string | null
   deletedAt: string | null
-  /*
-  id: number
-  username: string
-  password: string | undefined
-  email: string
-  first_name: string
-  last_name: string
-  fullname?: string
-  occupation?: string
-  companyName?: string
-  phone?: string
-  roles?: Array<number>
-  pic?: string
-  language?: 'en' | 'de' | 'es' | 'fr' | 'ja' | 'zh' | 'ru'
-  timeZone?: string
-  website?: 'https://keenthemes.com'
-  emailSettings?: UserEmailSettingsModel
-  auth?: AuthModel
-  communication?: UserCommunicationModel
-  address?: UserAddressModel
-  socialNetworks?: UserSocialNetworksModel
-  */
 }
