451d12a16bf6830ccf72d0ad047e4cdff879abe4
chihiro888 <93immm@naver.com>
2022-09-05 22:23:50 +0900

    feature: update ddl

--- infra/ddl/1_init_ddl.sql
+++ infra/ddl/1_init_ddl.sql
@@ -15,3 +15,20 @@ CREATE TABLE IF NOT EXISTS _user (
   `updated_at` DATETIME DEFAULT NULL COMMENT 'update time',
   `deleted_at` DATETIME DEFAULT NULL COMMENT 'delete time'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT="user";
+
+/*
+  query table
+*/
+DROP TABLE IF EXISTS _query;
+CREATE TABLE IF NOT EXISTS _query (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `type` VARCHAR(30) NOT NULL COMMENT 'type',
+  `exec_query` TEXT NOT NULL COMMENT 'execute query',
+  `success_cnt` INT NOT NULL COMMENT 'success count',
+  `fail_cnt` INT NOT NULL COMMENT 'fail count',
+  `user_id` BIGINT NOT NULL COMMENT 'user id',
+  `ip_address` VARCHAR(255) NOT NULL COMMENT 'ip address',
+  `created_at` DATETIME DEFAULT now() COMMENT 'create time',
+  `updated_at` DATETIME DEFAULT NULL COMMENT 'update time',
+  `deleted_at` DATETIME DEFAULT NULL COMMENT 'delete time'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT="query";
