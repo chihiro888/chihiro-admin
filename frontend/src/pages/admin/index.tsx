193fea95bc4ab482a6a2b3ee6f68d59d0b8868d7
chihiro888 <93immm@naver.com>
2023-01-04 11:37:51 +0900

    feat: update admin management

--- frontend/src/pages/admin/index.tsx
+++ frontend/src/pages/admin/index.tsx
@@ -6,7 +6,8 @@ import {
   Pagination,
   Snackbar,
   Stack,
-  TableContainer
+  TableContainer,
+  useTheme
 } from '@mui/material'
 import Paper from '@mui/material/Paper'
 import { useEffect, useState, Fragment } from 'react'
@@ -16,16 +17,30 @@ import moment from 'moment'
 import Chip from '@mui/material/Chip'
 
 // ** MUI Imports
+import Menu from '@mui/material/Menu'
+import CustomInput from 'src/pages/components/PickersCustomInput'
+import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
+import DatePicker, { ReactDatePickerProps } from 'react-datepicker'
+import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 import Dialog from '@mui/material/Dialog'
 import Typography from '@mui/material/Typography'
 import DialogTitle from '@mui/material/DialogTitle'
 import DialogContent from '@mui/material/DialogContent'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import InputLabel from '@mui/material/InputLabel'
+import FormControl from '@mui/material/FormControl'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
+import Divider from '@mui/material/Divider'
+import Collapse from '@mui/material/Collapse'
+import CardActions from '@mui/material/CardActions'
+import Box from '@mui/material/Box'
+import IconButton from '@mui/material/IconButton'
 
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
@@ -33,6 +48,64 @@ import DATE from 'src/common/constants/date'
 import { getPaginationCount } from 'src/utils'
 import CustomLottie from '../components/custom-lottie'
 import { getAdminList } from 'src/apis/admin'
+
+const RowOptions = ({ id }: { id: number | string }) => {
+  // ** State
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+  const rowOptionsOpen = Boolean(anchorEl)
+
+  const handleRowOptionsClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleRowOptionsClose = () => {
+    setAnchorEl(null)
+  }
+
+  const handleDelete = () => {
+    handleRowOptionsClose()
+  }
+
+  return (
+    <>
+      <IconButton size="small" onClick={handleRowOptionsClick}>
+        <Icon icon="bx:dots-vertical-rounded" />
+      </IconButton>
+      <Menu
+        keepMounted
+        anchorEl={anchorEl}
+        open={rowOptionsOpen}
+        onClose={handleRowOptionsClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right'
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right'
+        }}
+        PaperProps={{ style: { minWidth: '8rem' } }}
+      >
+        <MenuItem
+          component="a"
+          sx={{ '& svg': { mr: 2 } }}
+          onClick={handleRowOptionsClose}
+        >
+          <Icon icon="bx:show" fontSize={20} />
+          상세
+        </MenuItem>
+        <MenuItem onClick={handleRowOptionsClose} sx={{ '& svg': { mr: 2 } }}>
+          <Icon icon="bx:pencil" fontSize={20} />
+          수정
+        </MenuItem>
+        <MenuItem onClick={handleDelete} sx={{ '& svg': { mr: 2 } }}>
+          <Icon icon="bx:trash-alt" fontSize={20} />
+          삭제
+        </MenuItem>
+      </Menu>
+    </>
+  )
+}
 
 const Admin = () => {
   // ** State
@@ -45,11 +118,23 @@ const Admin = () => {
   const [open, setOpen] = useState<boolean>(false)
   const [detail, setDetail] = useState<any>()
 
+  const theme = useTheme()
+  const { direction } = theme
+  const [date, setDate] = useState(new Date())
+  const popperPlacement: ReactDatePickerProps['popperPlacement'] =
+    direction === 'ltr' ? 'bottom-start' : 'bottom-end'
+
+  const [collapse, setCollapse] = useState<boolean>(false)
+
   const [state, setState] = useState({
     openSnack: false,
     content: ''
   })
   const { openSnack, content } = state
+
+  const handleClickCollapse = () => {
+    setCollapse(!collapse)
+  }
 
   const handleClickSnack = (password: string) => {
     setState({
@@ -119,6 +204,91 @@ const Admin = () => {
         }
       />
 
+      <Stack sx={{ mt: 5 }}>
+        <div style={{ textAlign: 'right' }}>
+          <Button variant="contained">추가</Button>
+        </div>
+      </Stack>
+
+      <Card sx={{ mt: 5 }} style={{ overflow: 'unset' }}>
+        <CardContent style={{ padding: '0px' }}>
+          <CardActions className="card-action-dense">
+            <Box
+              sx={{
+                width: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+              }}
+            >
+              <Button onClick={handleClickCollapse}>검색필터</Button>
+              <IconButton size="small" onClick={handleClickCollapse}>
+                <Icon
+                  fontSize="1.875rem"
+                  icon={collapse ? 'bx:chevron-up' : 'bx:chevron-down'}
+                />
+              </IconButton>
+            </Box>
+          </CardActions>
+          <Collapse in={collapse}>
+            <Divider sx={{ m: '0 !important' }} />
+            <CardContent>
+              <Grid container spacing={4}>
+                <Grid item xs={3}>
+                  <FormControl style={{ width: '100%' }}>
+                    <TextField id="outlined-basic" label="계정" />
+                  </FormControl>
+                </Grid>
+
+                <Grid item xs={3}>
+                  <FormControl style={{ width: '100%' }}>
+                    <InputLabel id="demo-simple-select-outlined-label">
+                      권한
+                    </InputLabel>
+                    <Select
+                      label="level"
+                      defaultValue=""
+                      id="demo-simple-select-outlined"
+                      labelId="demo-simple-select-outlined-label"
+                      style={{ width: '100%' }}
+                    >
+                      <MenuItem value="">
+                        <em>전체</em>
+                      </MenuItem>
+                      <MenuItem value={10}>시스템관리자</MenuItem>
+                      <MenuItem value={20}>관리자</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+
+                <Grid item xs={3}>
+                  <FormControl style={{ width: '100%' }}>
+                    <DatePickerWrapper>
+                      <DatePicker
+                        selected={date}
+                        id="basic-input"
+                        popperPlacement={popperPlacement}
+                        onChange={(date: Date) => setDate(date)}
+                        customInput={<CustomInput label="생성일자" />}
+                        dateFormat="yyyy-MM-dd"
+                      />
+                    </DatePickerWrapper>
+                  </FormControl>
+                </Grid>
+              </Grid>
+              <Stack sx={{ mt: 5 }}>
+                <div style={{ textAlign: 'right' }}>
+                  <Button variant="outlined" color="secondary" sx={{ mr: 3 }}>
+                    초기화
+                  </Button>
+                  <Button variant="contained">검색</Button>
+                </div>
+              </Stack>
+            </CardContent>
+          </Collapse>
+        </CardContent>
+      </Card>
+
       {pagination.data.length === 0 ? (
         <>
           <CustomLottie text={'데이터가 존재하지 않습니다.'} />
@@ -139,6 +309,7 @@ const Admin = () => {
                         <TableCell>권한</TableCell>
                         <TableCell>생성일자</TableCell>
                         <TableCell>수정일자</TableCell>
+                        <TableCell>액션</TableCell>
                       </TableRow>
                     </TableHead>
                     <TableBody>
@@ -183,6 +354,9 @@ const Admin = () => {
                             {row?.updatedAt
                               ? moment(row?.updatedAt).format(DATE.DATETIME)
                               : '-'}
+                          </TableCell>
+                          <TableCell>
+                            <RowOptions />
                           </TableCell>
                         </TableRow>
                       ))}
