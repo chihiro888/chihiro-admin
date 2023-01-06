200c61f4248da308c8959d243e6e3783783c4b81
Inho Lee <mandeuk1014@gmail.com>
2023-01-06 13:28:40 +0900

    fix: dispatch occur multiple re-render problem

--- frontend/src/pages/admin/history.tsx
+++ frontend/src/pages/admin/history.tsx
@@ -20,112 +20,115 @@ import {
   setDetailAPI,
   setActionList
 } from 'src/store/apps/crud'
+import { useEffect } from 'react'
 
 const AdminHistory = () => {
   // ** Hooks
   const dispatch = useDispatch()
 
-  // NOTE 리스트 조회 API 정의
-  dispatch(setListAPI(getLoginHistoryList))
+  useEffect(() => {
+    // NOTE 리스트 조회 API 정의
+    dispatch(setListAPI(getLoginHistoryList))
 
-  // NOTE 상세 API 정의
-  dispatch(setDetailAPI(getLoginHistoryDetail))
+    // NOTE 상세 API 정의
+    dispatch(setDetailAPI(getLoginHistoryDetail))
 
-  // NOTE 페이지 헤더 정의
-  dispatch(
-    setPageHeader({
-      title: '로그인 이력',
-      subTitle: '로그인 이력을 확인할 수 있습니다.'
-    })
-  )
+    // NOTE 페이지 헤더 정의
+    dispatch(
+      setPageHeader({
+        title: '로그인 이력',
+        subTitle: '로그인 이력을 확인할 수 있습니다.'
+      })
+    )
 
-  // NOTE 테이블 헤더 정의
-  dispatch(
-    setTableHeader([
-      '아이디',
-      '사용자아이디',
-      '사용자계정',
-      '사용자명',
-      '타입',
-      '생성일자',
-      '수정일자',
-      '액션'
-    ])
-  )
+    // NOTE 테이블 헤더 정의
+    dispatch(
+      setTableHeader([
+        '아이디',
+        '사용자아이디',
+        '사용자계정',
+        '사용자명',
+        '타입',
+        '생성일자',
+        '수정일자',
+        '액션'
+      ])
+    )
 
-  // NOTE 상세 폼 설정
-  dispatch(
-    setDetailForm([
-      {
-        label: '아이디',
-        key: 'id',
-        value: ''
-      },
-      {
-        label: '사용자 아이디',
-        key: 'userId',
-        value: ''
-      },
-      {
-        label: '타입',
-        key: 'type',
-        value: ''
-      },
-      {
-        label: '생성일자',
-        key: 'createdAt',
-        value: ''
-      },
-      {
-        label: '수정일자',
-        key: 'updatedAt',
-        value: ''
-      },
-      ,
-      {
-        label: '삭제일자',
-        key: 'deletedAt',
-        value: ''
-      }
-    ])
-  )
+    // NOTE 상세 폼 설정
+    dispatch(
+      setDetailForm([
+        {
+          label: '아이디',
+          key: 'id',
+          value: ''
+        },
+        {
+          label: '사용자 아이디',
+          key: 'userId',
+          value: ''
+        },
+        {
+          label: '타입',
+          key: 'type',
+          value: ''
+        },
+        {
+          label: '생성일자',
+          key: 'createdAt',
+          value: ''
+        },
+        {
+          label: '수정일자',
+          key: 'updatedAt',
+          value: ''
+        },
+        ,
+        {
+          label: '삭제일자',
+          key: 'deletedAt',
+          value: ''
+        }
+      ])
+    )
 
-  // NOTE 검색 폼 설정
-  dispatch(
-    setSearchForm([
-      {
-        type: 'text',
-        label: '계정',
-        key: 'account',
-        value: ''
-      },
-      {
-        type: 'select',
-        label: '타입',
-        key: 'type',
-        value: '',
-        list: [
-          {
-            label: '로그인',
-            value: 1
-          },
-          {
-            label: '로그아웃',
-            value: 0
-          }
-        ]
-      },
-      {
-        type: 'date',
-        label: '생성일자',
-        key: 'createdAt',
-        value: ''
-      }
-    ])
-  )
+    // NOTE 검색 폼 설정
+    dispatch(
+      setSearchForm([
+        {
+          type: 'text',
+          label: '계정',
+          key: 'account',
+          value: ''
+        },
+        {
+          type: 'select',
+          label: '타입',
+          key: 'type',
+          value: '',
+          list: [
+            {
+              label: '로그인',
+              value: 1
+            },
+            {
+              label: '로그아웃',
+              value: 0
+            }
+          ]
+        },
+        {
+          type: 'date',
+          label: '생성일자',
+          key: 'createdAt',
+          value: ''
+        }
+      ])
+    )
 
-  // NOTE 액션 정의
-  dispatch(setActionList([]))
+    // NOTE 액션 정의
+    dispatch(setActionList([]))
+  }, [])
 
   return (
     <>
--- frontend/src/pages/admin/index.tsx
+++ frontend/src/pages/admin/index.tsx
@@ -32,243 +32,246 @@ import {
   setDeleteAPI,
   setActionList
 } from 'src/store/apps/crud'
+import { useEffect } from 'react'
 
 const Admin = () => {
   // ** Hooks
   const dispatch = useDispatch()
 
-  // NOTE 리스트 조회 API 정의
-  dispatch(setListAPI(getAdminList))
+  useEffect(() => {
+    // NOTE 리스트 조회 API 정의
+    dispatch(setListAPI(getAdminList))
 
-  // NOTE 생성 API 정의
-  dispatch(setCreateAPI(createAdmin))
+    // NOTE 생성 API 정의
+    dispatch(setCreateAPI(createAdmin))
 
-  // NOTE 상세 API 정의
-  dispatch(setDetailAPI(getAdminDetail))
+    // NOTE 상세 API 정의
+    dispatch(setDetailAPI(getAdminDetail))
 
-  // NOTE 삭제 API 정의
-  dispatch(setDeleteAPI(deleteAdmin))
+    // NOTE 삭제 API 정의
+    dispatch(setDeleteAPI(deleteAdmin))
 
-  // NOTE 페이지 헤더 정의
-  dispatch(
-    setPageHeader({
-      title: '관리자 관리',
-      subTitle: '관리자를 관리할 수 있습니다.'
-    })
-  )
+    // NOTE 페이지 헤더 정의
+    dispatch(
+      setPageHeader({
+        title: '관리자 관리',
+        subTitle: '관리자를 관리할 수 있습니다.'
+      })
+    )
 
-  // NOTE 테이블 헤더 정의
-  dispatch(
-    setTableHeader([
-      '아이디',
-      '계정',
-      '비밀번호',
-      '사용자명',
-      '권한',
-      '생성일자',
-      '수정일자',
-      '액션'
-    ])
-  )
+    // NOTE 테이블 헤더 정의
+    dispatch(
+      setTableHeader([
+        '아이디',
+        '계정',
+        '비밀번호',
+        '사용자명',
+        '권한',
+        '생성일자',
+        '수정일자',
+        '액션'
+      ])
+    )
 
-  // NOTE 추가 폼 설정
-  dispatch(
-    setAddForm([
-      {
-        type: 'text',
-        label: '계정',
-        key: 'account',
-        value: ''
-      },
-      {
-        type: 'password',
-        label: '비밀번호',
-        key: 'password',
-        value: ''
-      },
-      {
-        type: 'password',
-        label: '비밀번호 확인',
-        key: 'confirmPassword',
-        value: ''
-      },
-      {
-        type: 'text',
-        label: '사용자명',
-        key: 'username',
-        value: ''
-      },
-      {
-        type: 'select',
-        label: '권한',
-        key: 'level',
-        value: '',
-        list: [
-          {
-            label: '시스템 관리자',
-            value: 'SA'
-          },
-          {
-            label: '관리자',
-            value: 'A'
-          }
-        ]
-      }
-    ])
-  )
+    // NOTE 추가 폼 설정
+    dispatch(
+      setAddForm([
+        {
+          type: 'text',
+          label: '계정',
+          key: 'account',
+          value: ''
+        },
+        {
+          type: 'password',
+          label: '비밀번호',
+          key: 'password',
+          value: ''
+        },
+        {
+          type: 'password',
+          label: '비밀번호 확인',
+          key: 'confirmPassword',
+          value: ''
+        },
+        {
+          type: 'text',
+          label: '사용자명',
+          key: 'username',
+          value: ''
+        },
+        {
+          type: 'select',
+          label: '권한',
+          key: 'level',
+          value: '',
+          list: [
+            {
+              label: '시스템 관리자',
+              value: 'SA'
+            },
+            {
+              label: '관리자',
+              value: 'A'
+            }
+          ]
+        }
+      ])
+    )
 
-  // NOTE 상세 폼 설정
-  dispatch(
-    setDetailForm([
-      {
-        label: '아이디',
-        key: 'id',
-        value: ''
-      },
-      {
-        label: '계정',
-        key: 'account',
-        value: ''
-      },
-      {
-        label: '비밀번호',
-        key: 'password',
-        value: ''
-      },
-      {
-        label: '사용자명',
-        key: 'username',
-        value: ''
-      },
-      {
-        label: '시스템관리자',
-        key: 'isSystemAdmin',
-        value: ''
-      },
-      {
-        label: '관리자',
-        key: 'isAdmin',
-        value: ''
-      },
-      {
-        label: '생성일자',
-        key: 'createdAt',
-        value: ''
-      },
-      {
-        label: '수정일자',
-        key: 'updatedAt',
-        value: ''
-      },
-      ,
-      {
-        label: '삭제일자',
-        key: 'deletedAt',
-        value: ''
-      }
-    ])
-  )
+    // NOTE 상세 폼 설정
+    dispatch(
+      setDetailForm([
+        {
+          label: '아이디',
+          key: 'id',
+          value: ''
+        },
+        {
+          label: '계정',
+          key: 'account',
+          value: ''
+        },
+        {
+          label: '비밀번호',
+          key: 'password',
+          value: ''
+        },
+        {
+          label: '사용자명',
+          key: 'username',
+          value: ''
+        },
+        {
+          label: '시스템관리자',
+          key: 'isSystemAdmin',
+          value: ''
+        },
+        {
+          label: '관리자',
+          key: 'isAdmin',
+          value: ''
+        },
+        {
+          label: '생성일자',
+          key: 'createdAt',
+          value: ''
+        },
+        {
+          label: '수정일자',
+          key: 'updatedAt',
+          value: ''
+        },
+        ,
+        {
+          label: '삭제일자',
+          key: 'deletedAt',
+          value: ''
+        }
+      ])
+    )
 
-  // NOTE 검색 폼 설정
-  dispatch(
-    setSearchForm([
-      {
-        type: 'text',
-        label: '계정',
-        key: 'account',
-        value: ''
-      },
-      {
-        type: 'select',
-        label: '권한',
-        key: 'level',
-        value: '',
-        list: [
-          {
-            label: '시스템관리자',
-            value: 'SA'
-          },
-          {
-            label: '관리자',
-            value: 'A'
-          }
-        ]
-      },
-      {
-        type: 'date',
-        label: '생성일자',
-        key: 'createdAt',
-        value: ''
-      }
-    ])
-  )
+    // NOTE 검색 폼 설정
+    dispatch(
+      setSearchForm([
+        {
+          type: 'text',
+          label: '계정',
+          key: 'account',
+          value: ''
+        },
+        {
+          type: 'select',
+          label: '권한',
+          key: 'level',
+          value: '',
+          list: [
+            {
+              label: '시스템관리자',
+              value: 'SA'
+            },
+            {
+              label: '관리자',
+              value: 'A'
+            }
+          ]
+        },
+        {
+          type: 'date',
+          label: '생성일자',
+          key: 'createdAt',
+          value: ''
+        }
+      ])
+    )
 
-  // NOTE 액션 정의
-  dispatch(
-    setActionList([
-      {
-        icon: 'bx:pencil',
-        label: '비밀번호 변경',
-        content: [
-          {
-            type: 'password',
-            label: '기존 비밀번호',
-            key: 'oldPassword',
-            value: ''
-          },
-          {
-            type: 'password',
-            label: '새로운 비밀번호',
-            key: 'newPassword',
-            value: ''
-          },
-          {
-            type: 'password',
-            label: '새로운 비밀번호 확인',
-            key: 'confirmNewPassword',
-            value: ''
-          }
-        ],
-        updateAPI: updateAdminPassword
-      },
-      {
-        icon: 'bx:pencil',
-        label: '사용자명 변경',
-        content: [
-          {
-            type: 'text',
-            label: '사용자명',
-            key: 'username',
-            value: ''
-          }
-        ],
-        updateAPI: updateAdminUsername
-      },
-      {
-        icon: 'bx:pencil',
-        label: '권한 변경',
-        content: [
-          {
-            type: 'select',
-            label: '권한',
-            key: 'level',
-            value: '',
-            list: [
-              {
-                label: '시스템 관리자',
-                value: 'SA'
-              },
-              {
-                label: '관리자',
-                value: 'A'
-              }
-            ]
-          }
-        ],
-        updateAPI: updateAdminLevel
-      }
-    ])
-  )
+    // NOTE 액션 정의
+    dispatch(
+      setActionList([
+        {
+          icon: 'bx:pencil',
+          label: '비밀번호 변경',
+          content: [
+            {
+              type: 'password',
+              label: '기존 비밀번호',
+              key: 'oldPassword',
+              value: ''
+            },
+            {
+              type: 'password',
+              label: '새로운 비밀번호',
+              key: 'newPassword',
+              value: ''
+            },
+            {
+              type: 'password',
+              label: '새로운 비밀번호 확인',
+              key: 'confirmNewPassword',
+              value: ''
+            }
+          ],
+          updateAPI: updateAdminPassword
+        },
+        {
+          icon: 'bx:pencil',
+          label: '사용자명 변경',
+          content: [
+            {
+              type: 'text',
+              label: '사용자명',
+              key: 'username',
+              value: ''
+            }
+          ],
+          updateAPI: updateAdminUsername
+        },
+        {
+          icon: 'bx:pencil',
+          label: '권한 변경',
+          content: [
+            {
+              type: 'select',
+              label: '권한',
+              key: 'level',
+              value: '',
+              list: [
+                {
+                  label: '시스템 관리자',
+                  value: 'SA'
+                },
+                {
+                  label: '관리자',
+                  value: 'A'
+                }
+              ]
+            }
+          ],
+          updateAPI: updateAdminLevel
+        }
+      ])
+    )
+  }, [])
 
   return (
     <>
