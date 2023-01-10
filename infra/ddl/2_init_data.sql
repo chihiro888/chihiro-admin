9946c7eb41831fbfdccf25165c0e07e3b45665fc
chihiro888 <93immm@naver.com>
2023-01-10 19:23:30 +0900

    feat: update data

--- infra/ddl/2_init_data.sql
+++ infra/ddl/2_init_data.sql
@@ -1,5 +1,4 @@
 use develop;
 
--- password is '12341234'
-insert into _admin(account, password, username, is_system_admin, is_admin)
-values('chihiro888@github.com', '$2b$10$UDb8KzYXwwTSXLlpxr.NGef1PCq1cFS8DI7DVCbFQKuPw.02GbxNW', 'Chihiro', 1, 1);
+insert into _global(`key`,`value`,memo,created_at,updated_at,deleted_at) values 
+    ('imageDomain','http://localhost:7000','이미지 도메인',now(),null,null);
