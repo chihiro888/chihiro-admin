7fb1fc6aaed984f4ccad365d611f200206cce2fd
chihiro888 <93immm@naver.com>
2023-01-10 19:22:38 +0900

    feat: update module

--- backend/src/api/image/image.module.ts
+++ backend/src/api/image/image.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
+import { GlobalService } from '../global/global.service'
 import { ImageController } from './image.controller'
 import { ImageService } from './image.service'
 
 @Module({
   imports: [],
   controllers: [ImageController],
-  providers: [ImageService]
+  providers: [ImageService, GlobalService]
 })
 export class ImageModule {}
