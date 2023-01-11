fbabe53def10f0001f9ce997093b2580fac3a5e1
chihiro888 <93immm@naver.com>
2023-01-11 10:11:59 +0900

    feat: update file entity

--- backend/src/entities/file.entity.ts
+++ backend/src/entities/file.entity.ts
@@ -5,49 +5,31 @@ export class File {
   @PrimaryGeneratedColumn({ type: 'int', name: 'id', comment: 'id' })
   id: number
 
-  @Column('varchar', {
-    name: 'raw_name',
-    comment: 'original file name',
-    length: 255
-  })
+  @Column('varchar', { name: 'raw_name', comment: 'raw name', length: 255 })
   rawName: string
 
-  @Column('varchar', {
-    name: 'enc_name',
-    comment: 'encoded file name',
-    length: 255
-  })
+  @Column('varchar', { name: 'enc_name', comment: 'enc name', length: 255 })
   encName: string
 
-  @Column('varchar', {
-    name: 'extension',
-    comment: 'file extension',
-    length: 255
-  })
+  @Column('varchar', { name: 'extension', comment: 'extension', length: 255 })
   extension: string
 
-  @Column({ type: 'int', name: 'size', comment: 'file bytes size' })
+  @Column('int', { name: 'size', comment: 'size' })
   size: number
 
-  @Column('varchar', {
-    name: 'h_size',
-    comment: 'file human size',
-    length: 255
-  })
+  @Column('varchar', { name: 'h_size', comment: 'human size', length: 255 })
   hSize: string
 
-  @Column('varchar', {
-    name: 'abs_path',
-    comment: 'file absolute path',
-    length: 255
-  })
+  @Column('varchar', { name: 'abs_path', comment: 'abs path', length: 255 })
   absPath: string
 
+  @Column('varchar', { name: 'note', comment: 'note', length: 255 })
+  note: string
 
   @Column('datetime', {
     name: 'created_at',
     nullable: true,
-    comment: 'file upload time',
+    comment: 'create time',
     default: () => 'CURRENT_TIMESTAMP'
   })
   createdAt: String | Date | null
@@ -55,15 +37,14 @@ export class File {
   @Column('datetime', {
     name: 'updated_at',
     nullable: true,
-    comment: 'file updated time'
+    comment: 'update time'
   })
   updatedAt: String | Date | null
 
   @Column('datetime', {
     name: 'deleted_at',
     nullable: true,
-    comment: 'create time',
-    default: () => 'CURRENT_TIMESTAMP'
+    comment: 'delete time'
   })
   deletedAt: String | Date | null
 }
