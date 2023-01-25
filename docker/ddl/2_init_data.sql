da91c7ffcf3e33f3f9ecb6bbb1aeb9080250cb3e
chihiro888 <93immm@naver.com>
2023-01-25 22:31:37 +0900

    feat: update docker

--- docker/ddl/2_init_data.sql
+++ docker/ddl/2_init_data.sql
@@ -1,4 +1,4 @@
 use develop;
 
 insert into _global(`key`,`value`,memo,created_at,updated_at,deleted_at) values 
-    ('imageDomain','http://localhost:7000','이미지 도메인',now(),null,null);
+    ('imageDomain','http://localhost:5000','이미지 도메인',now(),null,null);
