0c72d6c049a1f0f28948ca274a982e263efb6233
chihiro888 <93immm@naver.com>
2023-01-26 10:53:09 +0900

    feat: update dashboard swagger

--- backend/src/api/dashboard/dashboard.controller.ts
+++ backend/src/api/dashboard/dashboard.controller.ts
@@ -10,7 +10,7 @@ import {
   Res,
   UseGuards
 } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { Response } from 'express'
 import { AuthGuard } from 'src/common/guard/auth.guard'
 import { DashboardService } from './dashboard.service'
@@ -24,6 +24,14 @@ export class DashboardController {
   // ANCHOR get admin count
   @UseGuards(AuthGuard)
   @Get('getAdminCount')
+  @ApiOperation({
+    summary: '관리자 카운트',
+    description: '관리자 카운트를 반환합니다.'
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '데이터 조회가 성공적인 경우 반환'
+  })
   async getAdminCount(@Res() res: Response) {
     const data = await this.dashboardService.getAdminCount()
 
@@ -38,6 +46,14 @@ export class DashboardController {
   // ANCHOR get login history count
   @UseGuards(AuthGuard)
   @Get('getLoginHistoryCount')
+  @ApiOperation({
+    summary: '로그인 이력 카운트',
+    description: '로그인 이력 카운트를 반환합니다.'
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '데이터 조회가 성공적인 경우 반환'
+  })
   async getLoginHistoryCount(@Res() res: Response) {
     const data = await this.dashboardService.getLoginHistoryCount()
 
@@ -52,6 +68,14 @@ export class DashboardController {
   // ANCHOR get image count
   @UseGuards(AuthGuard)
   @Get('getImageCount')
+  @ApiOperation({
+    summary: '이미지 카운트',
+    description: '이미지 카운트를 반환합니다.'
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '데이터 조회가 성공적인 경우 반환'
+  })
   async getImageCount(@Res() res: Response) {
     const data = await this.dashboardService.getImageCount()
 
@@ -66,6 +90,14 @@ export class DashboardController {
   // ANCHOR get setting count
   @UseGuards(AuthGuard)
   @Get('getSettingCount')
+  @ApiOperation({
+    summary: '설정 카운트',
+    description: '설정 카운트를 반환합니다.'
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '데이터 조회가 성공적인 경우 반환'
+  })
   async getSettingCount(@Res() res: Response) {
     const data = await this.dashboardService.getSettingCount()
 
