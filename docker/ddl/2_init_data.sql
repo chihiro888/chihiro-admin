8e48418d7f24f9e2ac39d29c9b35c7634b6adb74
chihiro888 <93immm@naver.com>
2023-03-07 21:56:22 +0900

    feat: init default logo

--- docker/ddl/2_init_data.sql
+++ docker/ddl/2_init_data.sql
@@ -7,7 +7,7 @@ insert into _global(`key`,`value`,memo,created_at,updated_at,deleted_at) values
     ('role','{"U": "사용자", "A": "관리자", "SA": "시스템관리자"}','권한 (JSON)',now(),null,null);    
 
 insert into _global(`key`,`value`,memo,created_at,updated_at,deleted_at) values 
-    ('appLogo','http://localhost:5001/b53ed845-7cde-4580-bd98-817d16d99f8c.png','앱 로고',now(),null,null);
+    ('appLogo','http://localhost:5001/9415e64f-5328-4706-909f-abc34fba1427.png','앱 로고',now(),null,null);
 
 insert into _global(`key`,`value`,memo,created_at,updated_at,deleted_at) values 
     ('appName','치히로 관리자','앱 이름',now(),null,null);
