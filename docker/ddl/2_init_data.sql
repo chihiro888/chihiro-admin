a078c4deb135dca9d28c34c08ef6fff2a117b6ce
chihiro888 <93immm@naver.com>
2023-03-08 10:09:09 +0900

    feat: update init data

--- docker/ddl/2_init_data.sql
+++ docker/ddl/2_init_data.sql
@@ -14,3 +14,6 @@ insert into _global(`key`,`value`,memo,created_at,updated_at,deleted_at) values
 
 insert into _global(`key`,`value`,memo,created_at,updated_at,deleted_at) values 
     ('appDesc','프로그래밍 하기 싫어요','앱 설명',now(),null,null);
+
+insert into `_file` (`abs_path`, `created_at`, `enc_name`, `extension`, `h_size`, `note`, `raw_name`, `size`, `table_name`, `table_pk`, `type`) 
+    values ('', now(), '9415e64f-5328-4706-909f-abc34fba1427.png', 'png', '17KB', 'logo', 'logo.png', 17454, NULL, NULL, NULL)
