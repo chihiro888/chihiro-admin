d155b24ca2d6d8140cd989555b43ee8b9784931c
chihiro888 <93immm@naver.com>
2023-03-08 11:57:18 +0900

    feat: update state property

--- frontend/src/pages/builder/page.tsx
+++ frontend/src/pages/builder/page.tsx
@@ -37,19 +37,19 @@ const Page = () => {
     subTitle: ''
   })
   const [listApi, setListApi] = useState({
-    disable: false,
+    checked: true,
     functionName: ''
   })
   const [createApi, setCreateApi] = useState({
-    disable: false,
+    checked: false,
     functionName: ''
   })
   const [detailApi, setDetailApi] = useState({
-    disable: false,
+    checked: false,
     functionName: ''
   })
   const [deleteApi, setDeleteApi] = useState({
-    disable: false,
+    checked: false,
     functionName: ''
   })
   const [tableHeader, setTableHeader] = useState('')
@@ -113,7 +113,14 @@ const Page = () => {
         <Typography variant="subtitle1">URL 설정</Typography>
         <Card sx={{ mt: 3 }}>
           <CardContent>
-            <TextField id="outlined-basic" label="URL" fullWidth size="small" />
+            <TextField
+              id="outlined-basic"
+              label="URL"
+              fullWidth
+              size="small"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+            />
           </CardContent>
         </Card>
       </Box>
@@ -129,6 +136,10 @@ const Page = () => {
                   label="페이지 제목"
                   fullWidth
                   size="small"
+                  value={pageHeader.title}
+                  onChange={(e) => {
+                    setPageHeader({ ...pageHeader, title: e.target.value })
+                  }}
                 />
               </Grid>
               <Grid item xs={12} md={8}>
@@ -137,6 +148,10 @@ const Page = () => {
                   label="페이지 설명"
                   fullWidth
                   size="small"
+                  value={pageHeader.subTitle}
+                  onChange={(e) => {
+                    setPageHeader({ ...pageHeader, subTitle: e.target.value })
+                  }}
                 />
               </Grid>
             </Grid>
@@ -151,25 +166,87 @@ const Page = () => {
             <Grid container spacing={5}>
               <Grid item xs={12} md={3}>
                 <FormControlLabel
-                  control={<Switch defaultChecked disabled />}
+                  control={
+                    <Switch
+                      disabled
+                      checked={listApi.checked}
+                      onChange={(e) => {
+                        setListApi({ ...listApi, checked: e.target.checked })
+                      }}
+                    />
+                  }
                   label="리스트 API (필수)"
                 />
               </Grid>
               <Grid item xs={12} md={3}>
                 <FormControlLabel
-                  control={<Switch color="success" />}
+                  control={
+                    <Switch
+                      color="success"
+                      checked={createApi.checked}
+                      onChange={(e) => {
+                        if (!e.target.checked) {
+                          setCreateApi({
+                            checked: e.target.checked,
+                            functionName: ''
+                          })
+                        } else {
+                          setCreateApi({
+                            ...createApi,
+                            checked: e.target.checked
+                          })
+                        }
+                      }}
+                    />
+                  }
                   label="생성 API"
                 />
               </Grid>
               <Grid item xs={12} md={3}>
                 <FormControlLabel
-                  control={<Switch color="warning" />}
+                  control={
+                    <Switch
+                      color="warning"
+                      checked={detailApi.checked}
+                      onChange={(e) => {
+                        if (!e.target.checked) {
+                          setDetailApi({
+                            checked: e.target.checked,
+                            functionName: ''
+                          })
+                        } else {
+                          setDetailApi({
+                            ...detailApi,
+                            checked: e.target.checked
+                          })
+                        }
+                      }}
+                    />
+                  }
                   label="상세 API"
                 />
               </Grid>
               <Grid item xs={12} md={3}>
                 <FormControlLabel
-                  control={<Switch color="error" />}
+                  control={
+                    <Switch
+                      color="error"
+                      checked={deleteApi.checked}
+                      onChange={(e) => {
+                        if (!e.target.checked) {
+                          setDeleteApi({
+                            checked: e.target.checked,
+                            functionName: ''
+                          })
+                        } else {
+                          setDeleteApi({
+                            ...deleteApi,
+                            checked: e.target.checked
+                          })
+                        }
+                      }}
+                    />
+                  }
                   label="삭제 API"
                 />
               </Grid>
@@ -181,6 +258,10 @@ const Page = () => {
                   label="리스트 API 함수명"
                   fullWidth
                   size="small"
+                  value={listApi.functionName}
+                  onChange={(e) => {
+                    setListApi({ ...listApi, functionName: e.target.value })
+                  }}
                 />
               </Grid>
               <Grid item xs={12} md={3}>
@@ -189,7 +270,14 @@ const Page = () => {
                   label="생성 API 함수명"
                   fullWidth
                   size="small"
-                  disabled
+                  disabled={createApi.checked ? false : true}
+                  value={createApi.functionName}
+                  onChange={(e) => {
+                    setCreateApi({
+                      ...createApi,
+                      functionName: e.target.value
+                    })
+                  }}
                 />
               </Grid>
               <Grid item xs={12} md={3}>
@@ -198,7 +286,14 @@ const Page = () => {
                   label="상세 API 함수명"
                   fullWidth
                   size="small"
-                  disabled
+                  disabled={detailApi.checked ? false : true}
+                  value={detailApi.functionName}
+                  onChange={(e) => {
+                    setDetailApi({
+                      ...detailApi,
+                      functionName: e.target.value
+                    })
+                  }}
                 />
               </Grid>
               <Grid item xs={12} md={3}>
@@ -207,7 +302,14 @@ const Page = () => {
                   label="삭제 API 함수명"
                   fullWidth
                   size="small"
-                  disabled
+                  disabled={deleteApi.checked ? false : true}
+                  value={deleteApi.functionName}
+                  onChange={(e) => {
+                    setDeleteApi({
+                      ...deleteApi,
+                      functionName: e.target.value
+                    })
+                  }}
                 />
               </Grid>
             </Grid>
