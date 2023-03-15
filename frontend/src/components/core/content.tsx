56bef664ffe68d736e7d1bc1698703f2874d0db1
Inho Lee <mandeuk1014@gmail.com>
2023-03-15 11:24:12 +0900

    feat: cell design change

--- frontend/src/components/core/content.tsx
+++ frontend/src/components/core/content.tsx
@@ -15,69 +15,42 @@ import { useSelector } from 'react-redux'
 import ActionContainer from './action-container'
 import { useState } from 'react'
 import { Chip } from '@mui/material'
+import ModalEditorViewerContainer from './modal-editor-viewer-container'
 
 const Content = () => {
   // ** Hooks
   const crud = useSelector((state: RootState) => state.crud)
   const pagination = crud.pagination
-  const contentForm = crud.contentForm
+  //const contentForm = crud.contentForm
   const actionList = crud.actionList
 
-  // const [contentForm, setContentForm] = useState([
-  //   {
-  //     key: 'key',
-  //     type: 'text' // text | date | editor | code | image | chip | snackbar
-  //   },
-  //   {
-  //     key: 'key2',
-  //     type: 'image', // text | date | editor | code | image | chip | snackbar
-  //     width: 30,
-  //     height: 30
-  //   },
-  //   {
-  //     key: 'date1',
-  //     type: 'date' // text | date | editor | code | image | chip | snackbar
-  //   },
-  //   {
-  //     key: 'chip',
-  //     type: 'chip', // text | date | editor | code | image | chip | snackbar
-  //     color: 'primary',
-  //     variant: 'outlined
-  //   }
-  // ])
+  // getPageByUrl
 
-  /*
-    count: 2
-    data: []
-
-    format: [
-      {
-        key: 'key'
-        type: 'text' // text | date | editor | code | image | chip
-      }
-
-
-      'key': {
-          type: 'text' // text | date | editor | code | image | chip
-        }
-      },
-      'url': {
-          type: 'image',
-          width: 30
-          height: 30
-        },
-      }
-      'createdAt': {
-          type: 'date'
-        }
-      }
-    ]
-    
-    action: {
-      detail: true,
-      delete: true
+  const [contentForm, setContentForm] = useState([
+    {
+      key: 'key',
+      type: 'text' // text | date | editor | code | image | chip | snackbar
+    },
+    {
+      key: 'profile',
+      type: 'image', // text | date | editor | code | image | chip | snackbar
+      width: 30,
+      height: 30
+    },
+    {
+      key: 'createdAt',
+      type: 'date' // text | date | editor | code | image | chip | snackbar
+    },
+    {
+      key: 'chip',
+      type: 'chip' // text | date | editor | code | image | chip | snackbar
+    },
+    {
+      key: 'intro',
+      type: 'modal', // text | date | editor | code | image | chip | snackbar
+      title: '자기소개'
     }
-  */
+  ])
 
   return (
     <>
@@ -91,22 +64,27 @@ const Content = () => {
                 <TableCell key={index}>
                   {cell.key ? moment(cell.key).format(DATE.DATETIME) : '-'}
                 </TableCell>
-              ) : cell.type === 'code' ? (
-                <TableCell key={index}>{row[`${cell.key}`]}</TableCell>
+              ) : cell.type === 'modal' ? (
+                <TableCell key={index}>
+                  <ModalEditorViewerContainer
+                    title={cell.title}
+                    content={row[`${cell.key}`]}
+                  />
+                </TableCell>
               ) : cell.type === 'image' ? (
                 <TableCell key={index}>
                   <img
-                    src={row[`${cell.key}`]}
-                    width={cell.width}
-                    height={cell.height}
+                    src={row[`${cell.key}`] || '/images/custom/image.png'}
+                    width={cell.width || 30}
+                    height={cell.height || 30}
                   />
                 </TableCell>
               ) : cell.type === 'chip' ? (
                 <TableCell key={index}>
                   <Chip
-                    label={row[`${cell.key}`]}
-                    color={cell.color}
-                    variant={cell.variant}
+                    label={row[`${cell.key}`] || cell.key}
+                    color="primary"
+                    variant="outlined"
                   />
                 </TableCell>
               ) : (
